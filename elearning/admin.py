95f83f502703eca24a8aed30d744888577560a23
Anggara Jauhari <r4f4.cool@gmail.com>
2014-09-21 22:54:00 +0700

    Comment for admin page

--- elearning/admin.py
+++ elearning/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from elearning.models import Anggota, Kursus, Materi, MateriIsi
 
+""" Memasukkan Model Anggota kedalam halaman admin """
 admin.site.register(Anggota)
+
+""" Memasukkan Model Kursus kedalam halaman admin """
 admin.site.register(Kursus)
+
+""" Memasukkan Model Materi kedalam halaman admin """
 admin.site.register(Materi)
+
+""" Memasukkan Model MateriIsi kedalam halaman admin """
 admin.site.register(MateriIsi)
